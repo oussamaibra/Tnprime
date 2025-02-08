9d421bc09aa7a727e0cb5ba4b4563b5d533c7169
oussamaibra <oussamabr58@gmail.com>
2025-02-08 14:25:51 +0100

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,6 +25,7 @@ import "swiper/components/pagination/pagination.min.css";
 import "swiper/components/scrollbar/scrollbar.min.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Script from "next/script";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
@@ -63,23 +64,43 @@ const MyApp = ({ Component, pageProps }: AppCustomProps) => {
       localStorage.setItem("location", JSON.stringify(res.data));
     }
   };
-  
+
   useEffect(() => {
     checkLocation();
   }, []);
 
   return (
-    <NextIntlProvider messages={pageProps?.messages}>
-      <ProvideAuth>
-        <ProvideWishlist>
-          <ProvideCart>
-            <Elements stripe={stripePromise} options={options}>
-              <Component {...pageProps} />
-            </Elements>
-          </ProvideCart>
-        </ProvideWishlist>
-      </ProvideAuth>
-    </NextIntlProvider>
+    <>
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1055542786588397');
+fbq('track', 'PageView');
+`,
+        }}
+      />
+      <NextIntlProvider messages={pageProps?.messages}>
+        <ProvideAuth>
+          <ProvideWishlist>
+            <ProvideCart>
+              <Elements stripe={stripePromise} options={options}>
+                <Component {...pageProps} />
+              </Elements>
+            </ProvideCart>
+          </ProvideWishlist>
+        </ProvideAuth>
+      </NextIntlProvider>
+    </>
   );
 };
 
