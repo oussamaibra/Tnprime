75269d68b364e1f21596775abceac57fede832b4
oussamaibra <oussamabr58@gmail.com>
2025-02-12 13:49:01 +0100

    fix pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -84,7 +84,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1055542786588397');
+fbq('init', '4240960932806829');
 fbq('track', 'PageView');
 `,
         }}
