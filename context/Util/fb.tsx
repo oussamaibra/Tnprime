e79041610ea6e290d9df61b2a8f53788d0fdd644
oussamaibra <oussamabr58@gmail.com>
2025-02-17 13:36:24 +0100

    fix

--- context/Util/fb.tsx
+++ context/Util/fb.tsx
@@ -1,51 +1,49 @@
 "use client";
-import React, { useEffect, useState } from "react";
 import _ from "lodash";
-import { roundDecimal } from "../../components/Util/utilFunc";
-import { useRouter } from "next/router";
+// import ReactPixel from "react-facebook-pixel";
 
-let ReactPixel: any = null;
+// let ReactPixel: any = null;
 
-if (typeof window !== "undefined") {
-  import("react-facebook-pixel")
-    .then((x) => x.default)
-    .then((currReactPixel) => {
-      ReactPixel = currReactPixel;
-    });
-}
+// if (typeof window !== "undefined") {
+//   import("react-facebook-pixel")
+//     .then((x) => x.default)
+//     .then((currReactPixel) => {
+//       ReactPixel = currReactPixel;
+//     });
+// }
 
-export const FacebookPixelEvents = () => {
-  const router = useRouter();
+// export const FacebookPixelEvents = () => {
+//   const router = useRouter();
 
-  // const pathname = usePathname();
-  // const searchParams = useSearchParams();
+//   // const pathname = usePathname();
+//   // const searchParams = useSearchParams();
 
-  useEffect(() => {
-    if (!ReactPixel) {
-      import("react-facebook-pixel")
-        .then((x) => x.default)
-        .then((currReactPixel) => {
-          ReactPixel = currReactPixel;
-        });
-      ReactPixel.init(`1302096797738899`);
-      ReactPixel.pageView();
-    } else {
-      ReactPixel.init(`1302096797738899`);
-      ReactPixel.pageView();
-    }
-  }, [router.pathname]);
+//   useEffect(() => {
+//     if (!ReactPixel) {
+//       import("react-facebook-pixel")
+//         .then((x) => x.default)
+//         .then((currReactPixel) => {
+//           ReactPixel = currReactPixel;
+//         });
+//       ReactPixel.init(`1302096797738899`);
+//       ReactPixel.pageView();
+//     } else {
+//       ReactPixel.init(`1302096797738899`);
+//       ReactPixel.pageView();
+//     }
+//   }, [router.pathname]);
 
-  return null;
-};
+//   return null;
+// };
 
 export const fbPixelAddToCart = async () => {
-  if (ReactPixel) ReactPixel.fbq("track", "AddToCart");
+  // if (ReactPixel) ReactPixel.fbq("track", "AddToCart");
 };
 
 export const fbPixelPurchase = async (total: number) => {
-  if (ReactPixel)
-    ReactPixel.fbq("track", "Purchase", {
-      currency: "USD",
-      value: Number(total),
-    });
+  // if (ReactPixel)
+  //   ReactPixel.fbq("track", "Purchase", {
+  //     currency: "USD",
+  //     value: Number(total),
+  //   });
 };
