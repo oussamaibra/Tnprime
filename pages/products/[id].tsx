055017729b65aec8c80c2e44026a067b21000c5b
oussamaibra <oussamabr58@gmail.com>
2025-02-25 15:04:44 +0100

    fix

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -599,6 +599,16 @@ const Product: React.FC<Props> = ({ product, products, url }) => {
           </div>
           <div className="infoSection w-full md:w-1/2 h-auto py-8 sm:pl-4 flex flex-col">
             <h1 className="text-3xl mb-4">{product.name}</h1>
+            
+            <span className="text-xl text-red mb-2 animate-pulse">
+              <Link href="/">
+                <a className="animate-pulse">
+                  {" "}
+                  👉 {t("See more products click here")} 👉
+                </a>
+              </Link>{" "}
+            </span>
+
             <span className="text-2xl text-gray400 mb-2">
               {productOption.price} {currency}
             </span>
@@ -635,9 +645,7 @@ const Product: React.FC<Props> = ({ product, products, url }) => {
                       handleSize(el);
                     }}
                     className={`flex items-center justify-center border ${
-                      size === el
-                        ? "border-red"
-                        : "border-gray300 text-gray400"
+                      size === el ? "border-red" : "border-gray300 text-gray400"
                     } cursor-pointer `}
                   >
                     <div>
